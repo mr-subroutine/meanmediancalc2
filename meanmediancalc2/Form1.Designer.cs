942f1cb4a3a481d96a18c160630aa71d5dc59a4b
Darryl S <darrylshack@outlook.com>
2020-07-09 10:41:05 -0700

    added functionality for median button

--- meanmediancalc2/Form1.Designer.cs
+++ meanmediancalc2/Form1.Designer.cs
@@ -34,39 +34,44 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btn_add = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_mean
             // 
-            this.btn_mean.Location = new System.Drawing.Point(12, 232);
+            this.btn_mean.Location = new System.Drawing.Point(12, 272);
             this.btn_mean.Name = "btn_mean";
             this.btn_mean.Size = new System.Drawing.Size(122, 36);
             this.btn_mean.TabIndex = 0;
             this.btn_mean.Text = "Calculate Mean";
             this.btn_mean.UseVisualStyleBackColor = true;
+            this.btn_mean.Click += new System.EventHandler(this.btn_mean_Click);
             // 
             // btn_median
             // 
-            this.btn_median.Location = new System.Drawing.Point(185, 232);
+            this.btn_median.Location = new System.Drawing.Point(185, 272);
             this.btn_median.Name = "btn_median";
             this.btn_median.Size = new System.Drawing.Size(122, 36);
             this.btn_median.TabIndex = 1;
             this.btn_median.Text = "Calculate Median";
             this.btn_median.UseVisualStyleBackColor = true;
+            this.btn_median.Click += new System.EventHandler(this.btn_median_Click);
             // 
             // btn_clear
             // 
-            this.btn_clear.Location = new System.Drawing.Point(232, 12);
+            this.btn_clear.Location = new System.Drawing.Point(217, 12);
             this.btn_clear.Name = "btn_clear";
-            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.Size = new System.Drawing.Size(90, 23);
             this.btn_clear.TabIndex = 3;
             this.btn_clear.Text = "Clear";
             this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 61);
+            this.label1.Location = new System.Drawing.Point(115, 84);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 4;
@@ -84,17 +89,37 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 77);
+            this.richTextBox1.Location = new System.Drawing.Point(93, 100);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(295, 125);
+            this.richTextBox1.Size = new System.Drawing.Size(136, 125);
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Save to File";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Load File";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(319, 280);
+            this.ClientSize = new System.Drawing.Size(484, 320);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btn_add);
             this.Controls.Add(this.label1);
@@ -118,6 +143,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_add;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- meanmediancalc2/Form1.cs
+++ meanmediancalc2/Form1.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace meanmediancalc2
 {
@@ -18,6 +19,7 @@ namespace meanmediancalc2
             InitializeComponent();
         }
 
+        // data structure for input
         List<int> enteredValues = new List<int>();
 
         private void Form1_Load(object sender, EventArgs e)
@@ -63,6 +65,7 @@ namespace meanmediancalc2
                     //MessageBox.Show("Value added");
                     enteredValues.Add(returnedValue);
                     writeToRichTextBox(returnedValue);
+                    enableButtons();
                 }
             }
         }
@@ -75,14 +78,83 @@ namespace meanmediancalc2
 
         private void enableButtons()
         {
-            btn_mean.Enabled = true;
-            btn_median.Enabled = true;
+            bool canCalculate = canBeCalulated();
+            if (canCalculate == true)
+            {
+                btn_mean.Enabled = true;
+                btn_median.Enabled = true;
+            }
+        }
+
+        private void disableButtons()
+        {
+            btn_mean.Enabled = false;
+            btn_median.Enabled = false;
         }
 
         // checks to see if list only has one item -- it needs at least two.
-        private void canBeCalulated()
+        private bool canBeCalulated()
         {
+            bool canCalculate = false;
+            int count = enteredValues.Count();
 
+            if (count > 1)
+            {
+                return canCalculate = true;
+            }
+
+            else
+            {
+                return canCalculate;
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            disableButtons();
+            richTextBox1.Clear();
+            enteredValues.Clear();
+        }
+
+        private void btn_mean_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            int avg = 0;
+            int count = enteredValues.Count();
+
+            for (int i = 0; i < enteredValues.Count; i++)
+            {
+                total += enteredValues[i];
+            }
+
+            avg = total / count;
+            MessageBox.Show("The mean is: " + avg);
+        }
+
+        private void btn_median_Click(object sender, EventArgs e)
+        {
+            int count = enteredValues.Count();
+            int median = 0;
+
+            if (count % 2 == 0)
+            {
+                enteredValues.Sort();
+                int pair1 = enteredValues[(count / 2) - 1];
+                int pair2 = enteredValues[(count / 2)];
+                median = (pair1 + pair2) / 2;
+                MessageBox.Show("The median is: " + median);
+                return;
+            }
+
+            else if (count % 2 != 0)
+            {
+                enteredValues.Sort();
+                count += 1;
+                median = enteredValues[(count / 2) - 1];
+
+                MessageBox.Show("The median is: " + median);
+                return;
+            }
         }
     }
 }
